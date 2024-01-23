5cf7a74f7b080742df134a87b199e09074f48d41
Anurag Shrestha <ashrestha@unm.edu>
2024-01-22 18:04:24 -0700

    Updated GameLoop class

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -1,7 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class GameLoop {
+
+public class GameLoop extends Application {
+
+
     public static void main(String[] args) {
+        launch(args);
+    }
 
-        System.out.printf("Hello and welcome!");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+        boardGrid board = new boardGrid();
+
+
+        Scene scene = new Scene(board,380,500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
